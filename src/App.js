bfa168028361b96814154d9770cd54351bd051b5
Jpenav0423 <jdpena0423@gmail.com>
2022-07-05 20:04:55 -0600

    Redux Saga App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getCatsFetch } from "./catState";
 
 function App() {
+
+  const cats = useSelector(state => state.cats.cats);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCatsFetch());
+  }, [dispatch]); 
+
+  console.log(cats);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>LA FERIA DE LOS GATOS</h1>
+      <p>Imagenes de gatos</p>
+      <hr />
+      <div className='Gallery'>
+        {cats.map( cat =>
+          <div key={cats.id} className="row">
+            <div className='columns column-left'>
+              <img 
+              alt={cat.name}
+              src={cat.image.url}
+              width="200"
+              height="200"
+              />
+            </div>
+            <div className="columns column-right">
+              <h2>{cat.name}</h2>
+              <h5>Temperament: {cat.temperament}</h5>
+              <p>{cat.description}</p>
+            </div>
+          </div>
+        )}
+
+      </div>
+    <button>View More</button>
     </div>
   );
 }
 
 export default App;
+
+
+/*
+{cats.map((cat, key) => {
+  return (
+    <h1 key={cat.id}>{cat.name}</h1>
+  )
+}
+  
+)}
+*/
